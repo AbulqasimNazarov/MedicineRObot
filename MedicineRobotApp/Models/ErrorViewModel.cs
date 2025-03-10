04fda8dd41967b8e31ece2832deb8d4020877e52
Abulqasim <abulqasim.nazarov@gmail.com>
2025-03-10 23:36:01 +0400

    Create project

---
+++ MedicineRobotApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MedicineRobotApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
